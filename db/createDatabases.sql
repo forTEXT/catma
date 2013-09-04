78067d584c3a749fe07327092a0c1ac93b20ba0c
Marco Petris <marco.petris@web.de>
2013-09-04 16:35:30 +0200

    config stuff

---
+++ db/createDatabases.sql
@@ -0,0 +1,25 @@
+-- phpMyAdmin SQL Dump
+-- version 3.3.7deb7
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Erstellungszeit: 16. August 2013 um 08:59
+-- Server Version: 5.5.25
+-- PHP-Version: 5.3.14-1~dotdeb.0
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Datenbank: `CatmaIndex`
+--
+CREATE DATABASE `CatmaIndex` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `CatmaIndex`;
+
+CREATE DATABASE `CatmaRepository` DEFAULT CHARACTER SET utf8 COLLATE utf8_bin;
+USE `CatmaRepository`;
