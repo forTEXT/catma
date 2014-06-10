70b98916bd6ab39043a02d998d049f76c923c2bb
Marco Petris <marco.petris@web.de>
2014-06-10 10:05:30 +0200

    prototyping

--- src/de/catma/indexer/db/CollocationSearcher2.java
+++ src/de/catma/indexer/db/CollocationSearcher2.java
@@ -87,118 +87,119 @@ public class CollocationSearcher2 {
 				final Set<QueryResultRow> baseQueryResultRows = entry.getValue();
 				
 				final Set<QueryResultRow> collocQueryResultRows = collocResultRowsByDocument.get(sourceDocumentId);
-				
-				ResourceIterable<Node> sourceDocNodes = 
-						graphDb.findNodesByLabelAndProperty(sourceDocLabel, "localUri", sourceDocumentId);
-				ResourceIterator<Node> iterator = sourceDocNodes.iterator();
-				Node sourceDocNode = iterator.next();
-				iterator.close();
-				
-				TraversalDescription positionsOfBaseQueryResultRowsTraversal = graphDb.traversalDescription()
-				.depthFirst()
-				.relationships(RelType.IS_PART_OF).relationships(RelType.HAS_POSITION, Direction.OUTGOING).relationships(RelType.ADJACENT_TO).evaluator(
-						new Evaluator() {
-							public Evaluation evaluate(Path path) {
-								Node endNode = path.endNode();
-								
-								if (endNode.hasLabel(positionLabel)) {
-									if (isInRange(baseQueryResultRows, endNode)) {
-										return Evaluation.INCLUDE_AND_CONTINUE;
+				if (collocQueryResultRows != null) {
+					ResourceIterable<Node> sourceDocNodes = 
+							graphDb.findNodesByLabelAndProperty(sourceDocLabel, "localUri", sourceDocumentId);
+					ResourceIterator<Node> iterator = sourceDocNodes.iterator();
+					Node sourceDocNode = iterator.next();
+					iterator.close();
+					
+					TraversalDescription positionsOfBaseQueryResultRowsTraversal = graphDb.traversalDescription()
+					.depthFirst()
+					.relationships(RelType.IS_PART_OF).relationships(RelType.HAS_POSITION, Direction.OUTGOING).relationships(RelType.ADJACENT_TO).evaluator(
+							new Evaluator() {
+								public Evaluation evaluate(Path path) {
+									Node endNode = path.endNode();
+									
+									if (endNode.hasLabel(positionLabel)) {
+										if (isInRange(baseQueryResultRows, endNode)) {
+											return Evaluation.INCLUDE_AND_CONTINUE;
+										}
+										else {
+											return Evaluation.EXCLUDE_AND_PRUNE;
+										}
 									}
-									else {
-										return Evaluation.EXCLUDE_AND_PRUNE;
-									}
+									
+									
+									
+									return Evaluation.EXCLUDE_AND_CONTINUE;
 								}
-								
-								
-								
-								return Evaluation.EXCLUDE_AND_CONTINUE;
-							}
-						});
-				
-				Traverser positionsOfBaseQueryResultRowsTraverser = positionsOfBaseQueryResultRowsTraversal.traverse(sourceDocNode);
-				Map<QueryResultRow, TreeSet<Node>> orderedBaseNodesByRow = 
-					createdOrderedNodesByRow(positionsOfBaseQueryResultRowsTraverser, baseQueryResultRows);
-				
-				TraversalDescription positionsOfCollocQueryResultRowsTraversal = graphDb.traversalDescription()
-				.depthFirst()
-				.relationships(RelType.IS_PART_OF).relationships(RelType.HAS_POSITION, Direction.OUTGOING).relationships(RelType.ADJACENT_TO).evaluator(
-						new Evaluator() {
-							public Evaluation evaluate(Path path) {
-								Node endNode = path.endNode();
-								
-								if (endNode.hasLabel(positionLabel)) {
-									if (isInRange(collocQueryResultRows, endNode)) {
-										return Evaluation.INCLUDE_AND_CONTINUE;
+							});
+					
+					Traverser positionsOfBaseQueryResultRowsTraverser = positionsOfBaseQueryResultRowsTraversal.traverse(sourceDocNode);
+					Map<QueryResultRow, TreeSet<Node>> orderedBaseNodesByRow = 
+						createdOrderedNodesByRow(positionsOfBaseQueryResultRowsTraverser, baseQueryResultRows);
+					
+					TraversalDescription positionsOfCollocQueryResultRowsTraversal = graphDb.traversalDescription()
+					.depthFirst()
+					.relationships(RelType.IS_PART_OF).relationships(RelType.HAS_POSITION, Direction.OUTGOING).relationships(RelType.ADJACENT_TO).evaluator(
+							new Evaluator() {
+								public Evaluation evaluate(Path path) {
+									Node endNode = path.endNode();
+									
+									if (endNode.hasLabel(positionLabel)) {
+										if (isInRange(collocQueryResultRows, endNode)) {
+											return Evaluation.INCLUDE_AND_CONTINUE;
+										}
+										else {
+											return Evaluation.EXCLUDE_AND_PRUNE;
+										}
 									}
-									else {
-										return Evaluation.EXCLUDE_AND_PRUNE;
-									}
+									
+									
+									
+									return Evaluation.EXCLUDE_AND_CONTINUE;
 								}
-								
-								
-								
-								return Evaluation.EXCLUDE_AND_CONTINUE;
-							}
-						});
-				
-				Traverser positionsOfCollocQueryResultRowsTraverser = positionsOfCollocQueryResultRowsTraversal.traverse(sourceDocNode);
-				Map<QueryResultRow, TreeSet<Node>> orderedCollocNodesByRow = 
-					createdOrderedNodesByRow(positionsOfCollocQueryResultRowsTraverser, collocQueryResultRows);
-
-				Set<List<TermInfo>> collocTermInfos = new HashSet<List<TermInfo>>();
-				
-				for (Map.Entry<QueryResultRow, TreeSet<Node>> collocNodesEntry : orderedCollocNodesByRow.entrySet()) {
-					ArrayList<TermInfo> curTermInfos = new ArrayList<TermInfo>();
-					for (Node n : collocNodesEntry.getValue()) {
-						curTermInfos.add(new TermInfo(
-								(String)n.getProperty("literal"),
-								(Integer)n.getProperty("start"),
-								(Integer)n.getProperty("end"),
-								(Integer)n.getProperty("position")));
-					}
-					collocTermInfos.add(curTermInfos);
-				}
-				
-				System.out.println(collocTermInfos);
-				
-				for (Map.Entry<QueryResultRow, TreeSet<Node>> orderedNodesEntry : orderedBaseNodesByRow.entrySet()) {
-					Node first = orderedNodesEntry.getValue().first();
-					Node last = orderedNodesEntry.getValue().last();
+							});
 					
-					SpanContext context = new SpanContext(sourceDocumentId);
-					for (Node n : getContext(first, spanContextSize, Direction.INCOMING)) {
-						context.addBackwardToken(
-							new TermInfo(
-								(String)n.getProperty("literal"),
-								(Integer)n.getProperty("start"),
-								(Integer)n.getProperty("end"),
-								(Integer)n.getProperty("position")));
-					}
-					for (Node n : getContext(last, spanContextSize, Direction.OUTGOING)) {
-						context.addForwardToken(
-							new TermInfo(
-								(String)n.getProperty("literal"),
-								(Integer)n.getProperty("start"),
-								(Integer)n.getProperty("end"),
-								(Integer)n.getProperty("position")));
-					}
+					Traverser positionsOfCollocQueryResultRowsTraverser = positionsOfCollocQueryResultRowsTraversal.traverse(sourceDocNode);
+					Map<QueryResultRow, TreeSet<Node>> orderedCollocNodesByRow = 
+						createdOrderedNodesByRow(positionsOfCollocQueryResultRowsTraverser, collocQueryResultRows);
+	
+					Set<List<TermInfo>> collocTermInfos = new HashSet<List<TermInfo>>();
 					
-					System.out.println(context);
-					
-					boolean found = false;
-					
-					for (List<TermInfo> collocTerms : collocTermInfos) {
-						if (context.contains(collocTerms)) {
-							found = true;
-							break;
+					for (Map.Entry<QueryResultRow, TreeSet<Node>> collocNodesEntry : orderedCollocNodesByRow.entrySet()) {
+						ArrayList<TermInfo> curTermInfos = new ArrayList<TermInfo>();
+						for (Node n : collocNodesEntry.getValue()) {
+							curTermInfos.add(new TermInfo(
+									(String)n.getProperty("literal"),
+									(Integer)n.getProperty("start"),
+									(Integer)n.getProperty("end"),
+									(Integer)n.getProperty("position")));
 						}
+						collocTermInfos.add(curTermInfos);
 					}
 					
-					if (found) {
-						searchResult.add(orderedNodesEntry.getKey());
-					}
+					System.out.println(collocTermInfos);
 					
+					for (Map.Entry<QueryResultRow, TreeSet<Node>> orderedNodesEntry : orderedBaseNodesByRow.entrySet()) {
+						Node first = orderedNodesEntry.getValue().first();
+						Node last = orderedNodesEntry.getValue().last();
+						
+						SpanContext context = new SpanContext(sourceDocumentId);
+						for (Node n : getContext(first, spanContextSize, Direction.INCOMING)) {
+							context.addBackwardToken(
+								new TermInfo(
+									(String)n.getProperty("literal"),
+									(Integer)n.getProperty("start"),
+									(Integer)n.getProperty("end"),
+									(Integer)n.getProperty("position")));
+						}
+						for (Node n : getContext(last, spanContextSize, Direction.OUTGOING)) {
+							context.addForwardToken(
+								new TermInfo(
+									(String)n.getProperty("literal"),
+									(Integer)n.getProperty("start"),
+									(Integer)n.getProperty("end"),
+									(Integer)n.getProperty("position")));
+						}
+						
+						System.out.println(context);
+						
+						boolean found = false;
+						
+						for (List<TermInfo> collocTerms : collocTermInfos) {
+							if (context.contains(collocTerms)) {
+								found = true;
+								break;
+							}
+						}
+						
+						if (found) {
+							searchResult.add(orderedNodesEntry.getKey());
+						}
+						
+					}
 				}
 			}
 			transaction.success();
