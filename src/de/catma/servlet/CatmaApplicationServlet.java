e298ca3ffce83c2ed77773ce95c9c5d9f6e4f97f
RichardLanders <richard@informationlogistics.co.za>
2014-09-19 14:45:23 +0200

    add google analytics tracking code

--- src/de/catma/servlet/CatmaApplicationServlet.java
+++ src/de/catma/servlet/CatmaApplicationServlet.java
@@ -78,6 +78,15 @@ public class CatmaApplicationServlet extends VaadinServlet {
 		}
 		@Override
 		public void modifyBootstrapPage(BootstrapPageResponse response) {
+			response.getDocument().head().append("<script>"
+					+ "(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){"
+					+ "(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),"
+					+ "m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)"
+					+ "})(window,document,'script','//www.google-analytics.com/analytics.js','ga');"
+					+ "ga('create', 'UA-38728736-8', 'auto');"
+					+ "ga('send', 'pageview');"
+					+ "</script>");
+			
 			for (CssLib lib : CssLib.values()) {
 				response.getDocument().head().append(
 					"<link rel=\"stylesheet\" href=\"" 
