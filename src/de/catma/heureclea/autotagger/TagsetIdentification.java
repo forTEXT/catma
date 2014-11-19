666112e5f6f7172d3d575f9e4b258ba2713dfcd3
Marco Petris <marco.petris@web.de>
2014-11-19 11:55:42 +0100

    temporal_signals_hybrid added

--- src/de/catma/heureclea/autotagger/TagsetIdentification.java
+++ src/de/catma/heureclea/autotagger/TagsetIdentification.java
@@ -3,7 +3,8 @@ package de.catma.heureclea.autotagger;
 public enum TagsetIdentification {
 	pos("Parts Of Speech"),
 	tense("Tense"),
-	temporal_signals("Temporal Signals")
+	temporal_signals("Temporal Signals (simple)"),
+	temporal_signals_hybrid("Temporal Signals (hybrid)"),
 	;
 	
 	private String description;
