88aefe94d8e9929937eecfa88e64b8951e882651
Marco Petris <marco.petris@web.de>
2015-03-23 20:46:01 +0100

    quickfix wildcard query

--- src/de/catma/indexer/graph/PathUtil.java
+++ src/de/catma/indexer/graph/PathUtil.java
@@ -5,6 +5,28 @@ import org.neo4j.graphdb.Path;
 
 class PathUtil {
 
+	/**
+	 * @param p
+	 * @param max
+	 * @return the Position node by going backwards positionNodeHops Position nodes
+	 */
+	public Node getPositionNodeBackwardsAt(Path p, int positionNodeHops) {
+		int pCount = 0;
+		for (Node n : p.reverseNodes()) {
+			if (n.hasLabel(NodeType.Position)) {
+				pCount++;
+			}
+			
+			if (pCount==positionNodeHops) {
+				return n;
+			}
+			
+		}
+		throw new IllegalStateException(
+			"unable to detect first Position node within " 
+					+ positionNodeHops + " hops window!");
+	}
+	
 	public Node getFirstPositionNode(Path p) {
 		return getFirstNode(p, NodeType.Position);
 	}
