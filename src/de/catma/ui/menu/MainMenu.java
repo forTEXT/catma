adf4256290f6ff6c02fe3906781627dfa00bae41
maltem-za <malte@informationlogistics.co.za>
2015-06-04 14:08:06 +0200

    Revert "add missing return statement to"
    This reverts commit fc9b7565a8dd9b271d5edb8725d4fb91b41b12ca.

--- src/de/catma/ui/menu/MainMenu.java
+++ src/de/catma/ui/menu/MainMenu.java
@@ -34,11 +34,10 @@ public class MainMenu extends MenuBar {
 			if (entry.getValue() == component) {
 				MenuItem menuItem = entry.getKey();
 				menuItem.getCommand().menuSelected(menuItem);
-				return;
 			}
 		}
 		
-		throw new IllegalArgumentException("Component not present in menu");
+//		throw new IllegalArgumentException("Component not present in menu");
 	}
 	
 	public PropertyChangeListener userChangeListener = 
