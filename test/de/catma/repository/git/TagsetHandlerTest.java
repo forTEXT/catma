091514b2b8d16ab34355523edd493af12af72da6
Matthew Beets <matthew@labs.ws>
2017-10-13 16:11:26 +0200

    add TagsetHandlerTest file

---
+++ test/de/catma/repository/git/TagsetHandlerTest.java
@@ -0,0 +1,26 @@
+package de.catma.repository.git;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TagsetHandlerTest {
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void create() throws Exception {
+	}
+
+	@Test
+	public void delete() throws Exception {
+	}
+
+}
