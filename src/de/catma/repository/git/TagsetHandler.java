92825a17542b13016ee034f4ebb7243c9b7a4668
Matthew Beets <matthew@labs.ws>
2017-10-13 16:09:52 +0200

    use correct interface

--- src/de/catma/repository/git/TagsetHandler.java
+++ src/de/catma/repository/git/TagsetHandler.java
@@ -1,20 +1,17 @@
 package de.catma.repository.git;
 
-import de.catma.repository.git.exceptions.SourceDocumentHandlerException;
-import de.catma.repository.git.interfaces.ISourceDocumentHandler;
-import de.catma.repository.git.model_wrappers.GitSourceDocumentInfo;
+import de.catma.repository.git.interfaces.ITagsetHandler;
 
 import javax.annotation.Nullable;
-import java.io.InputStream;
 
-public class TagsetHandler implements ISourceDocumentHandler {
+public class TagsetHandler implements ITagsetHandler {
 	@Override
-	public String insert(InputStream originalSourceDocumentStream, String originalSourceDocumentFileName, InputStream convertedSourceDocumentStream, String convertedSourceDocumentFileName, GitSourceDocumentInfo gitSourceDocumentInfo, @Nullable String sourceDocumentId, @Nullable String projectId) throws SourceDocumentHandlerException {
+	public String create(String name, String description) {
 		return null;
 	}
 
 	@Override
-	public void remove(String sourceDocumentId) throws SourceDocumentHandlerException {
+	public void delete(String tagsetId) {
 
 	}
 }
