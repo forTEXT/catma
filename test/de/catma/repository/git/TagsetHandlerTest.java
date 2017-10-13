ea5f44fdc7c4d37fcb1790c4c33576f794710c54
Matthew Beets <matthew@labs.ws>
2017-10-13 16:35:52 +0200

    more testing setup

--- test/de/catma/repository/git/TagsetHandlerTest.java
+++ test/de/catma/repository/git/TagsetHandlerTest.java
@@ -1,18 +1,63 @@
 package de.catma.repository.git;
 
+import de.catma.repository.db.DBUser;
+import de.catma.repository.git.managers.LocalGitRepositoryManager;
+import de.catma.repository.git.managers.RemoteGitServerManager;
+import de.catma.repository.git.managers.RemoteGitServerManagerTest;
+import org.gitlab4j.api.models.User;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.Properties;
+
 import static org.junit.Assert.*;
 
 public class TagsetHandlerTest {
+	private Properties catmaProperties;
+	private RemoteGitServerManager remoteGitServerManager;
+	private LocalGitRepositoryManager localGitRepositoryManager;
+	private TagsetHandler tagsetHandler;
+
+	private File createdRepositoryPath = null;
+
+	public TagsetHandlerTest() throws Exception {
+		String propertiesFile = System.getProperties().containsKey("prop") ?
+				System.getProperties().getProperty("prop") : "catma.properties";
+
+		this.catmaProperties = new Properties();
+		this.catmaProperties.load(new FileInputStream(propertiesFile));
+	}
+
 	@Before
 	public void setUp() throws Exception {
+		// create a fake CATMA user which we'll use to instantiate the RemoteGitServerManager
+		de.catma.user.User catmaUser = new DBUser(
+			1, "catma-testuser", false, false, false
+		);
+
+		this.remoteGitServerManager = new RemoteGitServerManager(this.catmaProperties, catmaUser);
+		this.remoteGitServerManager.replaceGitLabServerUrl = true;
+
+		this.localGitRepositoryManager = new LocalGitRepositoryManager(this.catmaProperties);
+
+		this.tagsetHandler = new TagsetHandler(
+			this.localGitRepositoryManager, this.remoteGitServerManager
+		);
 	}
 
 	@After
 	public void tearDown() throws Exception {
+
+		// delete the GitLab user that the RemoteGitServerManager constructor in setUp would have
+		// created - see RemoteGitServerManagerTest tearDown() for more info
+		User user = this.remoteGitServerManager.getGitLabUser();
+		this.remoteGitServerManager.getAdminGitLabApi().getUserApi().deleteUser(user.getId());
+		RemoteGitServerManagerTest.awaitUserDeleted(
+			this.remoteGitServerManager.getAdminGitLabApi().getUserApi(), user.getId()
+		);
 	}
 
 	@Test
