569ca996bc4b2cdc0c6c2fb29ed688d0f12a811d
Malte Meister <malte@labs.ws>
2017-10-18 12:04:48 +0200

    add MarkupCollectionHandlerException class

---
+++ src/de/catma/repository/git/exceptions/MarkupCollectionHandlerException.java
@@ -0,0 +1,11 @@
+package de.catma.repository.git.exceptions;
+
+public class MarkupCollectionHandlerException extends Exception {
+	public MarkupCollectionHandlerException(String message) {
+		super(message);
+	}
+
+	public MarkupCollectionHandlerException(String message, Throwable cause) {
+		super(message, cause);
+	}
+}
