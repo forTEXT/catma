12448c9267d9a20183d4105756f645788cb6a9f3
Malte Meister <malte@labs.ws>
2017-10-25 12:09:30 +0200

    add assertions to toTagReferenceList test in JsonLdWebAnnotationTest

--- test/de/catma/repository/git/serialization/models/json_ld/JsonLdWebAnnotationTest.java
+++ test/de/catma/repository/git/serialization/models/json_ld/JsonLdWebAnnotationTest.java
@@ -10,6 +10,7 @@ import mockit.integration.junit4.JMockit;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -147,6 +148,63 @@ public class JsonLdWebAnnotationTest {
 
 		assertEquals(2, tagReferences.size());
 
-		// TODO: add assertions
+		for (TagReference tagReference : tagReferences) {
+			TagDefinition tagDefinition = tagReference.getTagDefinition();
+			TagInstance tagInstance = tagReference.getTagInstance();
+
+			assertEquals("CATMA_TAG_DEF", tagDefinition.getUuid());
+			assertEquals("TAG_DEF", tagDefinition.getName());
+			assertEquals("", tagDefinition.getParentUuid());
+
+			PropertyDefinition[] systemPropertyDefinitions = tagDefinition.getSystemPropertyDefinitions()
+					.toArray(new PropertyDefinition[0]);
+			assertEquals(1, systemPropertyDefinitions.length);
+			assertEquals("CATMA_SYSPROP_DEF", systemPropertyDefinitions[0].getUuid());
+			assertEquals("SYSPROP_DEF", systemPropertyDefinitions[0].getName());
+			List<String> possibleSystemPropertyValues = systemPropertyDefinitions[0].getPossibleValueList()
+					.getPropertyValueList().getValues();
+			assertEquals(2, possibleSystemPropertyValues.size());
+			assertArrayEquals(
+					new String[]{"SYSPROP_VAL_1", "SYSPROP_VAL_2"}, possibleSystemPropertyValues.toArray(new String[0])
+			);
+
+			PropertyDefinition[] userPropertyDefinitions = tagDefinition.getUserDefinedPropertyDefinitions()
+					.toArray(new PropertyDefinition[0]);
+			assertEquals(1, userPropertyDefinitions.length);
+			assertEquals("CATMA_UPROP_DEF", userPropertyDefinitions[0].getUuid());
+			assertEquals("UPROP_DEF", userPropertyDefinitions[0].getName());
+			List<String> possibleUserPropertyValues = userPropertyDefinitions[0].getPossibleValueList()
+					.getPropertyValueList().getValues();
+			assertEquals(2, possibleUserPropertyValues.size());
+			assertArrayEquals(
+					new String[]{"UPROP_VAL_1", "UPROP_VAL_2"}, possibleUserPropertyValues.toArray(new String[0])
+			);
+
+			assertEquals("CATMA_TAG_INST", tagInstance.getUuid());
+
+			Property[] systemProperties = tagInstance.getSystemProperties().toArray(new Property[0]);
+			assertEquals(1, systemProperties.length);
+			assertEquals(systemPropertyDefinitions[0], systemProperties[0].getPropertyDefinition());
+			List<String> systemPropertyValues = systemProperties[0].getPropertyValueList().getValues();
+			assertEquals(1, systemPropertyValues.size());
+			assertEquals("SYSPROP_VAL_1", systemPropertyValues.get(0));
+
+			Property[] userProperties = tagInstance.getUserDefinedProperties().toArray(new Property[0]);
+			assertEquals(1, userProperties.length);
+			assertEquals(userPropertyDefinitions[0], userProperties[0].getPropertyDefinition());
+			List<String> userPropertyValues = userProperties[0].getPropertyValueList().getValues();
+			assertEquals(1, userPropertyValues.size());
+			assertEquals("UPROP_VAL_2", userPropertyValues.get(0));
+		}
+
+		assertEquals(
+			new URI("http://catma.de/portal/sourcedocument/CATMA_SOURCEDOC"), tagReferences.get(0).getTarget()
+		);
+		assertEquals(new Range(12, 18), tagReferences.get(0).getRange());
+
+		assertEquals(
+			new URI("http://catma.de/portal/sourcedocument/CATMA_SOURCEDOC"), tagReferences.get(1).getTarget()
+		);
+		assertEquals(new Range(41, 47), tagReferences.get(1).getRange());
 	}
 }
