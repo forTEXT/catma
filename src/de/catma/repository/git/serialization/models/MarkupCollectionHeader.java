64c8e6dc480fd5107aa05cac0d0190a5149a6401
Matthew Beets <matthew@labs.ws>
2017-10-25 15:54:34 +0200

    add extra properties to MarkupCollectionHeader to rebuild the ContentInfoSet for the UserMarkupCollection

--- src/de/catma/repository/git/serialization/models/MarkupCollectionHeader.java
+++ src/de/catma/repository/git/serialization/models/MarkupCollectionHeader.java
@@ -3,9 +3,21 @@ package de.catma.repository.git.serialization.models;
 public class MarkupCollectionHeader extends HeaderBase {
 	private String sourceDocumentId;
 
+	private String author;
+	private String publisher;
+
+	public MarkupCollectionHeader(){
+		super("", "");
+		this.sourceDocumentId = "";
+		this.author = "";
+		this.publisher = "";
+	}
+
 	public MarkupCollectionHeader(String name, String description, String sourceDocumentId) {
 		super(name, description);
 		this.sourceDocumentId = sourceDocumentId;
+		this.author = "";
+		this.publisher = "";
 	}
 
 	public String getSourceDocumentId() {
@@ -15,4 +27,12 @@ public class MarkupCollectionHeader extends HeaderBase {
 	public void setSourceDocumentId(String sourceDocumentId) {
 		this.sourceDocumentId = sourceDocumentId;
 	}
+
+	public String getAuthor(){return this.author;}
+
+	public void setAuthor(String author){ this.author = author; }
+
+	public String getPublisher(){ return this.publisher; }
+
+	public void setPublisher(String publisher) { this.publisher = publisher; }
 }
