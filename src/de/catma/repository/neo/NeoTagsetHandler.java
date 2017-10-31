169172e26d73e425eed0d01ff0cdb21818cb5f95
Matthew Beets <matthew@labs.ws>
2017-10-31 15:33:03 +0200

    expand test; Add graphManager to NeoTagsetHandler;

--- src/de/catma/repository/neo/NeoTagsetHandler.java
+++ src/de/catma/repository/neo/NeoTagsetHandler.java
@@ -1,13 +1,16 @@
 package de.catma.repository.neo;
 
 import de.catma.repository.neo.exceptions.NeoTagsetHandlerException;
+import de.catma.repository.neo.managers.Neo4JGraphManager;
 import de.catma.tag.TagsetDefinition;
 import de.catma.util.IDGenerator;
 
 public class NeoTagsetHandler {
+	private final Neo4JGraphManager graphManager;
 	private final IDGenerator idGenerator;
 
-	public NeoTagsetHandler(){
+	public NeoTagsetHandler(Neo4JGraphManager graphManager){
+		this.graphManager = graphManager;
 		this.idGenerator = new IDGenerator();
 	}
 
--- test/de/catma/repository/neo/NeoTagsetHandlerTest.java
+++ test/de/catma/repository/neo/NeoTagsetHandlerTest.java
@@ -1,24 +1,40 @@
 package de.catma.repository.neo;
 
 import de.catma.repository.neo.exceptions.NeoTagsetHandlerException;
+import de.catma.repository.neo.managers.Neo4JGraphManager;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
+import java.io.FileInputStream;
+import java.util.Properties;
+
 import static org.junit.Assert.*;
 
 public class NeoTagsetHandlerTest {
+	private Properties catmaProperties;
+
+	public NeoTagsetHandlerTest() throws Exception {
+		String propertiesFile = System.getProperties().containsKey("prop") ?
+				System.getProperties().getProperty("prop") : "catma.properties";
+
+		this.catmaProperties = new Properties();
+		this.catmaProperties.load(new FileInputStream(propertiesFile));
+	}
 	// how to test for exceptions: https://stackoverflow.com/a/31826781
 	@Rule
 	public ExpectedException thrown = ExpectedException.none();
 
 	@Test
 	public void insertTagset() throws Exception {
-		NeoTagsetHandler neoTagsetHandler = new NeoTagsetHandler();
+		try (Neo4JGraphManager graphManager = new Neo4JGraphManager(this.catmaProperties)) {
 
-		thrown.expect(NeoTagsetHandlerException.class);
-		thrown.expectMessage("Not implemented");
-		neoTagsetHandler.insertTagset(null);
+			NeoTagsetHandler neoTagsetHandler = new NeoTagsetHandler(graphManager);
+
+			thrown.expect(NeoTagsetHandlerException.class);
+			thrown.expectMessage("Not implemented");
+			neoTagsetHandler.insertTagset(null);
+		}
 	}
 
 }
