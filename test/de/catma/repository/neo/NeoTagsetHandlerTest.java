e2710ca17795a019e56816a9b8f97281d6013fbd
Matthew Beets <matthew@labs.ws>
2017-11-03 13:11:14 +0200

    test showing usage

--- test/de/catma/repository/neo/NeoTagsetHandlerTest.java
+++ test/de/catma/repository/neo/NeoTagsetHandlerTest.java
@@ -2,6 +2,9 @@ package de.catma.repository.neo;
 
 import de.catma.repository.neo.exceptions.NeoTagsetHandlerException;
 import de.catma.repository.neo.managers.Neo4JGraphManager;
+import de.catma.repository.neo.managers.Neo4JOGMSessionFactory;
+import de.catma.repository.neo.serialization.models.Neo4JTagsetDefinition;
+import de.catma.tag.TagDefinition;
 import de.catma.tag.TagsetDefinition;
 import de.catma.util.IDGenerator;
 import org.junit.After;
@@ -35,7 +38,7 @@ public class NeoTagsetHandlerTest {
 		try (Neo4JGraphManager graphManager = new Neo4JGraphManager(this.catmaProperties)) {
 			Session session = graphManager.openSession();
 
-			session.run("MATCH (n) DETACH DELETE n");
+//			session.run("MATCH (n) DETACH DELETE n");
 		}
 	}
 
@@ -56,6 +59,66 @@ public class NeoTagsetHandlerTest {
 			tagsetDefinition.setName("ALovelyName");
 
 			neoTagsetHandler.insertTagset(tagsetDefinition);
+		}
+	}
+
+	@Test
+	public void insertTagsetWithTagDefinitions() throws Exception {
+		try (Neo4JGraphManager graphManager = new Neo4JGraphManager(this.catmaProperties)) {
+
+			NeoTagsetHandler neoTagsetHandler = new NeoTagsetHandler(graphManager);
+
+			String uuid = this.idGenerator.generate();
+
+			TagsetDefinition tagsetDefinition = new TagsetDefinition();
+			tagsetDefinition.setUuid(uuid);
+			tagsetDefinition.setName("ALovelyName");
+
+			TagDefinition tagDefinition1 = new TagDefinition();
+			tagDefinition1.setUuid("CATMA_TagDefinition1");
+			tagDefinition1.setName("TagsetDefinition1");
+
+			TagDefinition tagDefinition2 = new TagDefinition();
+			tagDefinition2.setUuid("CATMA_TagDefinition2");
+			tagDefinition2.setParentUuid("CATMA_TagDefinition1");
+			tagDefinition2.setName("TagsetDefinition2");
+
+			tagsetDefinition.addTagDefinition(tagDefinition1);
+			tagsetDefinition.addTagDefinition(tagDefinition2);
+
+			neoTagsetHandler.insertTagset(tagsetDefinition);
+		}
+	}
+
+	@Test
+	public void ogmInsertTagsetWithTagDefinitions() throws Exception {
+		try (Neo4JOGMSessionFactory neo4JOGMSessionFactory = new Neo4JOGMSessionFactory(this.catmaProperties)) {
+
+			String uuid = this.idGenerator.generate();
+
+			TagsetDefinition tagsetDefinition = new TagsetDefinition();
+			tagsetDefinition.setUuid(uuid);
+			tagsetDefinition.setName("ALovelyName");
+
+			TagDefinition tagDefinition1 = new TagDefinition();
+			tagDefinition1.setUuid("CATMA_TagDefinition1");
+			tagDefinition1.setName("TagsetDefinition1");
+
+			TagDefinition tagDefinition2 = new TagDefinition();
+			tagDefinition2.setUuid("CATMA_TagDefinition2");
+			tagDefinition2.setParentUuid("CATMA_TagDefinition1");
+			tagDefinition2.setName("TagsetDefinition2");
+
+			tagsetDefinition.addTagDefinition(tagDefinition1);
+			tagsetDefinition.addTagDefinition(tagDefinition2);
+
+//			neoTagsetHandler.insertTagset(tagsetDefinition);
+
+			Neo4JTagsetDefinition neo4JTagsetDefinition = new Neo4JTagsetDefinition(tagsetDefinition);
+
+			org.neo4j.ogm.session.Session session = neo4JOGMSessionFactory.getSession();
+
+			session.save(neo4JTagsetDefinition);
 		}
 	}
 
