27b9af8b66e3c431016481cb55314d4cc711aebd
Matthew Beets <matthew@labs.ws>
2017-11-03 13:10:16 +0200

    add new Neo4JOGMSessionFactory

---
+++ src/de/catma/repository/neo/managers/Neo4JOGMSessionFactory.java
@@ -0,0 +1,42 @@
+package de.catma.repository.neo.managers;
+
+import org.neo4j.ogm.config.Configuration;
+import org.neo4j.ogm.session.Session;
+import org.neo4j.ogm.session.SessionFactory;
+
+import java.util.Properties;
+
+public class Neo4JOGMSessionFactory implements AutoCloseable {
+	private SessionFactory sessionFactory;
+
+	public Neo4JOGMSessionFactory(String uri, String userName, String password){
+		Configuration configuration = new Configuration.Builder()
+				.uri(uri)
+				.credentials(userName, password)
+				.build();
+
+		this.sessionFactory = new SessionFactory(configuration,"de.catma.repository.neo");
+		sessionFactory.close();
+	}
+
+	public Neo4JOGMSessionFactory(Properties catmaProperties){
+		this(
+				catmaProperties.getProperty("Neo4JBoltUrl"),
+				catmaProperties.getProperty("Neo4JAdminUser"),
+				catmaProperties.getProperty("Neo4JAdminPassword")
+		);
+	}
+
+	public Session getSession(){
+		return this.sessionFactory.openSession();
+	}
+
+	@Override
+	public void close() throws Exception {
+		SessionFactory safeFactory = this.sessionFactory;
+		if (safeFactory != null) {
+			safeFactory.close();
+			this.sessionFactory = null;
+		}
+	}
+}
