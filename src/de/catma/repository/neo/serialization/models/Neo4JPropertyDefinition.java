2bb5485a41ca560d5e4360ef0882ec76b10f9fce
Matthew Beets <matthew@labs.ws>
2017-11-03 17:36:26 +0200

    add new wrapper class for PropertyDefinitions

---
+++ src/de/catma/repository/neo/serialization/models/Neo4JPropertyDefinition.java
@@ -0,0 +1,48 @@
+package de.catma.repository.neo.serialization.models;
+
+import de.catma.tag.PropertyDefinition;
+import de.catma.tag.PropertyPossibleValueList;
+import org.neo4j.ogm.annotation.Id;
+import org.neo4j.ogm.annotation.NodeEntity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@NodeEntity(label="PropertyDefinition")
+public class Neo4JPropertyDefinition {
+
+	@Id
+	private String uuid;
+	private String name;
+	private boolean singleSelect;
+	private List<String> possibleValues;
+
+	public Neo4JPropertyDefinition(){
+		this.singleSelect = true;
+		this.possibleValues = new ArrayList<>();
+	}
+
+	public Neo4JPropertyDefinition(PropertyDefinition propertyDefinition){
+		this();
+
+		this.setPropertyDefinition(propertyDefinition);
+	}
+
+	public PropertyDefinition getPropertyDefinition(){
+		PropertyDefinition propertyDefinition = new PropertyDefinition();
+
+		propertyDefinition.setUuid(this.uuid);
+		propertyDefinition.setName(this.name);
+		propertyDefinition.setPossibleValueList(new PropertyPossibleValueList(this.possibleValues, this.singleSelect));
+
+		return propertyDefinition;
+	}
+
+	public void setPropertyDefinition(PropertyDefinition propertyDefinition){
+		this.uuid = propertyDefinition.getUuid();
+		this.name = propertyDefinition.getName();
+		this.singleSelect = propertyDefinition.getPossibleValueList().isSingleSelect();
+
+		this.possibleValues.addAll(propertyDefinition.getPossibleValueList().getPropertyValueList().getValues());
+	}
+}
