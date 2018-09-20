936acc9f42615901dd0616c67afd81df51cef4e3
Dennis Berger <db@bsdsystems.de>
2018-09-20 17:42:48 +0200

    render views inside the section element. e.g. don't blindly append to body tag

--- src/main/java/de/catma/v10ui/modules/main/MainView.java
+++ src/main/java/de/catma/v10ui/modules/main/MainView.java
@@ -2,11 +2,14 @@ package de.catma.v10ui.modules.main;
 
 import com.vaadin.flow.component.*;
 import com.vaadin.flow.component.html.*;
+import com.vaadin.flow.dom.Element;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.router.RouterLayout;
 import de.catma.v10ui.routing.Routes;
 import de.catma.v10ui.util.Version;
+
+import java.util.Objects;
 
 /**
  * Main entrypoint for catma, it renders a navigation and a dashboard
@@ -21,6 +24,7 @@ public class MainView extends Div implements RouterLayout, HasComponents {
      * Keep version information in VCS
      */
     private final Header header = new Header(new H2("Catma " + Version.LATEST));
+
     /**
      * dashboard is the main section of catma
      */
@@ -36,7 +40,6 @@ public class MainView extends Div implements RouterLayout, HasComponents {
         initComponents();
     }
 
-
     /**
      * initialize all components
      */
@@ -44,7 +47,16 @@ public class MainView extends Div implements RouterLayout, HasComponents {
         this.add(header);
         this.add(navigation);
         this.add(dashboard);
-
     }
 
+    /**
+     * render the content inside the section element. e.g. don't append to blindly to body element.
+     * The implemenation of adding is copied from {@link RouterLayout#showRouterLayoutContent(HasElement)}
+     * 
+     * @param content
+     */
+    @Override
+    public void showRouterLayoutContent(HasElement content) {
+        dashboard.getElement().appendChild(new Element[]{(Element)Objects.requireNonNull(content.getElement())});
+    }
 }
