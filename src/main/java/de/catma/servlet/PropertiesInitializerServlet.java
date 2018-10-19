b27d170a20e10a842309eb32758752eb419159c1
christian bruck <c.bruck@gmx.de>
2018-10-19 10:13:38 +0200

    PropertiesInitianlizerServlet restored to older version

--- src/main/java/de/catma/servlet/PropertiesInitializerServlet.java
+++ src/main/java/de/catma/servlet/PropertiesInitializerServlet.java
@@ -1,19 +1,15 @@
 package de.catma.servlet;
 
-import java.io.File;
 import java.io.FileInputStream;
-import java.io.InputStream;
 import java.util.Properties;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 
 import de.catma.document.repository.RepositoryProperties;
 import de.catma.util.NonModifiableProperties;
 
-@WebServlet(name = "CATMAProperties", urlPatterns = "/CATMAProperties", loadOnStartup = 1)
 public class PropertiesInitializerServlet extends HttpServlet {
 	
     @Override
@@ -27,11 +23,12 @@ public class PropertiesInitializerServlet extends HttpServlet {
 					System.getProperties().containsKey("prop") ? System.getProperties().getProperty(
 					"prop") : "catma.properties";
 
-            ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
-            InputStream input = classLoader.getResourceAsStream(propertiesFile);
-            Properties properties = new Properties();
-			properties.load(input);
-
+			Properties properties = new Properties();
+	
+			properties.load(
+				new FileInputStream(
+					cfg.getServletContext().getRealPath(propertiesFile)));
+			
 			RepositoryProperties.INSTANCE.setProperties( 
 				new NonModifiableProperties(properties));
 			
