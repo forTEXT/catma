7362a0515bc1d32512b7fddbb0ea532a25a1c196
Marco Petris <marco.petris@web.de>
2018-10-26 15:42:41 +0200

    integration and example context menu

--- src/main/java/de/catma/v10ui/components/actiongrid/ActionGridBar.java
+++ src/main/java/de/catma/v10ui/components/actiongrid/ActionGridBar.java
@@ -1,8 +1,11 @@
 package de.catma.v10ui.components.actiongrid;
 
 import com.vaadin.flow.component.*;
+import com.vaadin.flow.component.contextmenu.ContextMenu;
 import com.vaadin.flow.component.html.NativeButton;
+import com.vaadin.flow.component.icon.Icon;
 import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.FlexComponent;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.shared.Registration;
@@ -20,9 +23,15 @@ public class ActionGridBar extends Composite<HorizontalLayout> {
     private IconButton btnSearch;
     private IconButton btnAdd;
     private IconButton btnMoreOptions;
+    private ContextMenu ctmAdd;
+    private ContextMenu ctmSearch;
+    private ContextMenu ctmMoreOptions;
 
     public ActionGridBar(Component titleComponent){
         this.titleComponent = titleComponent;
+        btnSearch = new IconButton( VaadinIcon.SEARCH.create(),(evt) -> {});
+        btnAdd = new IconButton( VaadinIcon.PLUS.create(),(evt) -> {});
+        btnMoreOptions = new IconButton(VaadinIcon.ELLIPSIS_DOTS_V.create(), (evt) -> {});
     }
 
     @Override
@@ -32,10 +41,6 @@ public class ActionGridBar extends Composite<HorizontalLayout> {
         content.addClassName(".actiongrid__bar");
         content.setSpacing(false);
         content.add(titleComponent);
-        btnSearch = new IconButton( VaadinIcon.SEARCH.create(),(evt) -> {});
-        btnAdd = new IconButton( VaadinIcon.PLUS.create(),(evt) -> {});
-        btnMoreOptions = new IconButton(VaadinIcon.ELLIPSIS_DOTS_V.create(), (evt) -> {});
-
         content.add(btnSearch);
         content.add(btnAdd);
         content.add(btnMoreOptions);
@@ -44,6 +49,26 @@ public class ActionGridBar extends Composite<HorizontalLayout> {
         return content;
     }
 
+    public ContextMenu getBtnAddContextMenu() {
+        if (this.ctmAdd == null) {
+            this.ctmAdd = new ContextMenu(btnAdd);
+        }
+        return this.ctmAdd;
+    }
+
+    public ContextMenu getBtnSearchContextMenu() {
+        if (this.ctmSearch == null) {
+            this.ctmSearch = new ContextMenu(btnSearch);
+        }
+        return this.ctmSearch;
+    }
+
+    public ContextMenu getBtnMoreOptionsContextMenu() {
+        if (this.ctmMoreOptions == null) {
+            this.ctmMoreOptions = new ContextMenu(btnMoreOptions);
+        }
+        return this.ctmMoreOptions;
+    }
     public Registration addBtnAddClickListener(ComponentEventListener<ClickEvent<NativeButton>> listener){
        return btnAdd.addClickListener(listener);
     }
@@ -56,4 +81,5 @@ public class ActionGridBar extends Composite<HorizontalLayout> {
     public Registration addBtnMoreOptionsClickListener(ComponentEventListener<ClickEvent<NativeButton>> listener){
         return btnMoreOptions.addClickListener(listener);
     }
+
 }
