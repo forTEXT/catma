91d04fb06cf2374145ac2ef8229a704f8cb53c59
christian bruck <c.bruck@gmx.de>
2018-11-29 11:55:00 +0100

    show and hide resultlist in resultpanel

--- src/main/java/de/catma/ui/analyzenew/ResultPanelNew.java
+++ src/main/java/de/catma/ui/analyzenew/ResultPanelNew.java
@@ -1,14 +1,21 @@
 package de.catma.ui.analyzenew;
 
+import com.vaadin.icons.VaadinIcons;
 import com.vaadin.server.FontAwesome;
 import com.vaadin.shared.ui.MarginInfo;
+import com.vaadin.ui.AbstractOrderedLayout;
 import com.vaadin.ui.Alignment;
+import com.vaadin.ui.Button;
 import com.vaadin.ui.Grid;
 import com.vaadin.ui.HorizontalLayout;
+import com.vaadin.ui.HorizontalSplitPanel;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.Panel;
 import com.vaadin.ui.TextArea;
 import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.Button.ClickEvent;
+import com.vaadin.ui.Button.ClickListener;
+import com.vaadin.ui.themes.ValoTheme;
 import com.vaadin.v7.data.Item;
 import com.vaadin.v7.shared.ui.label.ContentMode;
 import com.vaadin.v7.ui.Table;
@@ -18,96 +25,110 @@ import de.catma.queryengine.result.QueryResultRow;
 import de.catma.queryengine.result.QueryResultRowArray;
 
 public class ResultPanelNew extends Panel {
-VerticalLayout contentVerticalLayout ;
-Table queryResultTable;
-Grid<QueryResultRow> queryResultGrid ;
-TextArea textArea;
-Label queryInfo;
-HorizontalLayout iconBar;
-private Label caretDownIcon;
-private Label optionsIcon;
-private Label trashIcon;
 
-public ResultPanelNew(QueryResult result, String queryAsString)  {
-	
-	queryInfo = new Label(queryAsString);
- 
- 	iconBar = new HorizontalLayout();
- 	iconBar.setMargin(false);
+	private VerticalLayout contentVerticalLayout;
+	private Table queryResultTable;
+	private Grid<QueryResultRow> queryResultGrid;
+	private TextArea textArea;
+	private Label queryInfo;
+	private HorizontalLayout groupedIcons;
+	private Button caretDownBt;
+	private Button caretUpBt;
+	private Button trashBt;
+	private Button optionsBt;
 
- 
- 	contentVerticalLayout = new VerticalLayout();
- 	caretDownIcon = new Label();
- 	caretDownIcon.setContentMode(com.vaadin.shared.ui.ContentMode.HTML);
-	caretDownIcon.setValue(FontAwesome.CARET_DOWN.getHtml());
-	
-	optionsIcon = new Label();
-	optionsIcon.setContentMode(com.vaadin.shared.ui.ContentMode.HTML);
-	optionsIcon.setValue(FontAwesome.ELLIPSIS_V.getHtml());
-	
-	trashIcon = new Label();
-	trashIcon.setContentMode(com.vaadin.shared.ui.ContentMode.HTML);
-	trashIcon.setValue(FontAwesome.TRASH.getHtml());
-	
-	iconBar.addComponents(trashIcon,optionsIcon,caretDownIcon);
-	iconBar.setComponentAlignment(trashIcon,Alignment.MIDDLE_RIGHT);
+	private QueryResult queryResult;
+	private String queryAsString;
 
- 
-	
-    QueryResultRowArray resultRowArrayArrayList=   result.asQueryResultRowArray();
-	
+	public ResultPanelNew(QueryResult result, String queryAsString) {
+		this.queryResult = result;
+		this.queryAsString = queryAsString;
 
-    queryResultTable = new Table("Results");
-    queryResultTable.addContainerProperty("Phrase", String.class, null);
-    queryResultTable.addContainerProperty("Range", String.class, null);
+		initComponents();
+		initListeners();
+		setData();
 
-
-	 for (QueryResultRow queryResultRow : resultRowArrayArrayList) {
-		 Object newItemId = queryResultTable.addItem();
-		 Item row1 = queryResultTable.getItem(newItemId);
-		 
-		 row1.getItemProperty("Phrase").setValue(queryResultRow.getPhrase());
-		 row1.getItemProperty("Range").setValue(queryResultRow.getRange().toString());
-	
-		//queryResultTable.addItem( new Object[]{queryResultRow.getPhrase(),queryResultRow.getRange().toString()});
 	}
-	 
+	private void initComponents() {
+		contentVerticalLayout = new VerticalLayout();
+		setContent(contentVerticalLayout);
+		createResultInfoBar();
+		createButtonBar();
+	}
+	
+	private void initListeners() {
+		caretDownBt.addClickListener(new ClickListener() {
+			
+			public void buttonClick(ClickEvent event) {
+				contentVerticalLayout.addComponent(queryResultTable);
+				groupedIcons.replaceComponent(caretDownBt,caretUpBt);
+			
+			}
+		});
+		
+	caretUpBt.addClickListener(new ClickListener() {
+			
+			public void buttonClick(ClickEvent event) {
+				contentVerticalLayout.removeComponent(queryResultTable);
+				groupedIcons.replaceComponent(caretUpBt,caretDownBt);
+			
+			}
+		});
+	}
+
+	private void setData() {
+		QueryResultRowArray resultRowArrayArrayList = queryResult.asQueryResultRowArray();
+		queryResultTable = new Table("Results");
+		queryResultTable.addContainerProperty("Phrase", String.class, null);
+		queryResultTable.addContainerProperty("Range", String.class, null);
+
+		for (QueryResultRow queryResultRow : resultRowArrayArrayList) {
+			Object newItemId = queryResultTable.addItem();
+			Item row1 = queryResultTable.getItem(newItemId);
+
+			row1.getItemProperty("Phrase").setValue(queryResultRow.getPhrase());
+			row1.getItemProperty("Range").setValue(queryResultRow.getRange().toString());
+		}
+		queryResultTable.setWidth("100%");
+		//contentVerticalLayout.addComponent(queryResultTable);
+	
+	}
 
 
-    queryResultTable.setWidth("100%");
-	
-	contentVerticalLayout.addComponent(queryInfo);
-    contentVerticalLayout.addComponent(iconBar);
-	contentVerticalLayout.addComponent(queryResultTable);
-	
 
-	//contentVerticalLayout.setWidth("100%");
-	
-	setContent(contentVerticalLayout);
-	//setWidth("100%");
+	private void createResultInfoBar() {
+		QueryResultRowArray resultRowArrayArrayList = queryResult.asQueryResultRowArray();
+		int resultSize = resultRowArrayArrayList.size();
+		queryInfo = new Label(queryAsString + "(" + resultSize + ")");
+		contentVerticalLayout.addComponent(queryInfo);
+	}
 
-	
-}
-private void setData() {
-	
-}
-private void initComponents() {
-	
-}
+	private void createButtonBar() {
+		groupedIcons = new HorizontalLayout();
+		groupedIcons.setMargin(false);
 
-private void createInfoBar() {
-	
-}
-private void createbuttonsBar() {
-	
-}
-private void showResult() {
-	//arrowupIcon. set Listener.Listener..Listener
-	//contentVerticalLayout.addComponent(table,)
-}
-private void handleArrawUpDownAction() {
-	
-}
-	
+		caretDownBt = new Button(VaadinIcons.CARET_DOWN);
+		caretDownBt.addStyleName(ValoTheme.BUTTON_BORDERLESS);
+		caretUpBt = new Button(VaadinIcons.CARET_UP);
+		caretUpBt.addStyleName(ValoTheme.BUTTON_BORDERLESS);
+		optionsBt = new Button(VaadinIcons.ELLIPSIS_V);
+		optionsBt.addStyleName(ValoTheme.BUTTON_BORDERLESS);
+		trashBt = new Button(VaadinIcons.TRASH);
+		trashBt.addStyleName(ValoTheme.BUTTON_BORDERLESS);
+
+		groupedIcons.addComponents(trashBt, optionsBt, caretDownBt);
+		groupedIcons.setWidthUndefined();
+
+		contentVerticalLayout.addComponent(groupedIcons);
+		contentVerticalLayout.setComponentAlignment(groupedIcons, Alignment.MIDDLE_RIGHT);
+	}
+
+	private void showResult() {
+
+	}
+
+	private void handleArrawUpAndDownAction() {
+
+	}
 
 }
