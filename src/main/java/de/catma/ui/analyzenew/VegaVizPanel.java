3f97fddb67ce850467c79786a33f7c38e3b05571
christian bruck <c.bruck@gmx.de>
2019-01-07 09:04:19 +0100

    new Viz interface

---
+++ src/main/java/de/catma/ui/analyzenew/VegaVizPanel.java
@@ -0,0 +1,11 @@
+package de.catma.ui.analyzenew;
+
+public class VegaVizPanel implements VizPanel {
+
+	@Override
+	public void setQueryResults() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
