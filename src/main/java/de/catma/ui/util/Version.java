24c1c08c10c1ed48e77a22eebb9568b1f6ba20da
Marco Petris <marco.petris@web.de>
2019-12-09 08:47:26 +0100

    M 6.0.8

--- src/main/java/de/catma/ui/util/Version.java
+++ src/main/java/de/catma/ui/util/Version.java
@@ -13,10 +13,10 @@ public final class Version {
     private int patch;
 
 
-    public final static Version CATMA_6_0_7 = new Version(6, 0, 7);
+    public final static Version CATMA_6_0_8 = new Version(6, 0, 8);
 
 
-    public final static Version LATEST = CATMA_6_0_7;
+    public final static Version LATEST = CATMA_6_0_8;
 
 
     /**
