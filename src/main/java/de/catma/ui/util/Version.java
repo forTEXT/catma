9d78c0124d2aad0eb8c455468b61c79790ccd1a7
Marco Petris <marco.petris@web.de>
2020-05-20 19:31:36 +0200

    M 6.0.19

--- src/main/java/de/catma/ui/util/Version.java
+++ src/main/java/de/catma/ui/util/Version.java
@@ -13,10 +13,10 @@ public final class Version {
     private int patch;
 
 
-    public final static Version CATMA_6_0_18 = new Version(6, 0, 18);
+    public final static Version CATMA_6_0_19 = new Version(6, 0, 19);
 
 
-    public final static Version LATEST = CATMA_6_0_18;
+    public final static Version LATEST = CATMA_6_0_19;
 
 
     /**
