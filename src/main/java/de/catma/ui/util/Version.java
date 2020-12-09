ad1b06e3afd984b1f6acb234c2fea5cf713d0273
Malte Meister <malte@labs.ws>
2020-12-09 22:02:27 +0200

    update version to 6.1.3

--- src/main/java/de/catma/ui/util/Version.java
+++ src/main/java/de/catma/ui/util/Version.java
@@ -13,10 +13,10 @@ public final class Version {
     private int patch;
 
 
-    public final static Version CATMA_6_1_2 = new Version(6, 1, 2);
+    public final static Version CATMA_6_1_3 = new Version(6, 1, 3);
 
 
-    public final static Version LATEST = CATMA_6_1_2;
+    public final static Version LATEST = CATMA_6_1_3;
 
 
     /**
