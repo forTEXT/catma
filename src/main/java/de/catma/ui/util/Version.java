e2c34ccedb8239d06ff18b0da642206383721276
Malte Meister <malte.meister@gmx.net>
2023-09-15 10:56:49 +0200

    update version to 6.5.6

--- src/main/java/de/catma/ui/util/Version.java
+++ src/main/java/de/catma/ui/util/Version.java
@@ -13,10 +13,10 @@ public final class Version {
     private int patch;
 
 
-    public final static Version CATMA_6_5_5 = new Version(6, 5, 5);
+    public final static Version CATMA_6_5_6 = new Version(6, 5, 6);
 
 
-    public final static Version LATEST = CATMA_6_5_5;
+    public final static Version LATEST = CATMA_6_5_6;
 
 
     /**
