5061c3b7dfb3ee64932971645e52dd9d0bcc405b
Guillaume Est Une Palourde <gbeaulieu@koumbit.org>
2025-01-10 16:38:53 -0500

    Disable the possibility of saving a tag in a different tagset
    It would have been an easy feature to add, but I won't have time to finish it

--- src/main/java/de/catma/ui/module/tags/AbstractAddEditTagDialog.java
+++ src/main/java/de/catma/ui/module/tags/AbstractAddEditTagDialog.java
@@ -300,6 +300,7 @@ public abstract class AbstractAddEditTagDialog<T> extends AbstractOkCancelDialog
 		}
 		Optional<TagsetDefinition> otsd = Optional.of(lib.getTagsetDefinition(editedTag.getTagsetDefinitionUuid()));
 		initComponents(availableTagsets, otsd, allowPropertyDefEditing);
+		cbTagsets.setEnabled(false);
 		cbTagsets.addValueChangeListener(event -> {
 			if (cbTagsets.getSelectedItem().isPresent()) {
 				ArrayList<TagsetDefinition> theList = new ArrayList<TagsetDefinition>();
