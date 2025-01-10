88e2eeb921b158d44718949e89fd4718bbfdfcf0
Guillaume Est Une Palourde <gbeaulieu@koumbit.org>
2025-01-10 16:22:32 -0500

    Moving item in the hierarchy works in a tagset.

--- src/main/java/de/catma/ui/module/annotate/annotationpanel/AnnotationPanel.java
+++ src/main/java/de/catma/ui/module/annotate/annotationpanel/AnnotationPanel.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.logging.Logger;
 import java.util.Objects;
 import java.util.Optional;
 import java.util.Set;
@@ -82,7 +83,7 @@ public class AnnotationPanel extends VerticalLayout {
 		public void tagReferenceSelectionChanged(
 				List<TagReference> tagReferences, boolean selected);
 	}
-	
+	private static final Logger logger = Logger.getLogger(AnnotationPanel.class.getName());
 	private ComboBox<AnnotationCollection> currentEditableCollectionBox;
 	private Button btAddCollection;
 	private TreeGrid<TagsetTreeItem> tagsetGrid;
@@ -216,7 +217,46 @@ public class AnnotationPanel extends VerticalLayout {
 		project.getTagManager().addPropertyChangeListener(
 				TagManagerEvent.tagDefinitionChanged, 
 				tagChangedListener);
-		
+
+		PropertyChangeListener tagDefinitionMovedListener = new PropertyChangeListener() {
+			public void propertyChange(final PropertyChangeEvent evt) {
+				@SuppressWarnings("unchecked")
+				final Pair<TagsetDefinition, TagDefinition> newVal = (Pair<TagsetDefinition, TagDefinition>) evt.getNewValue();
+				final Pair<TagsetDefinition, TagDefinition> oldVal = (Pair<TagsetDefinition, TagDefinition>) evt.getOldValue();
+
+				System.out.println("inside the tag moved listener for the annotation panel");
+				TagsetDefinition fromTagset = newVal.getFirst();
+				TagDefinition fromTag = newVal.getSecond();
+				TagsetDefinition toTagset = newVal.getFirst();
+				TagDefinition toTag = newVal.getSecond();
+				Optional<TagsetDataItem> optionalTsdiTo = tagsetData.getRootItems().stream()
+							.map(tagsetTreeItem -> (TagsetDataItem)tagsetTreeItem)
+							.filter(tdi -> tdi.getTagset().getUuid().equals(toTagset.getUuid()))
+							.findFirst();
+				if (!optionalTsdiTo.isPresent()) {
+					logger.warning(	String.format( "Failed to find tagset with ID %1$s (to) in the AnnotationPanel TreeGrid for project \"%2$s\" with ID %3$s",
+						toTagset.getUuid(), project.getName(), project.getId()));
+					return;
+				}
+				TagsetDataItem tsdiTo = optionalTsdiTo.get();
+				TagDataItem tdiTo = new TagDataItem(toTag);
+				tdiTo.setPropertiesExpanded(true);
+
+				tagsetData.removeItem(tdiTo);
+
+				String toParent = toTag.getParentUuid();
+				TagsetTreeItem parentUpdateTo = tsdiTo;
+				if (!toParent.isEmpty()) {
+					parentUpdateTo = new TagDataItem(toTagset.getTagDefinition(toParent));
+				}
+				tagsetData.addItem(parentUpdateTo, tdiTo);
+				addTagSubTree(toTagset, toTag, tdiTo);
+				tagsetDataProvider.refreshAll();
+				showExpandedProperties(tdiTo);
+			}
+		};
+		project.getTagManager().addPropertyChangeListener(TagManagerEvent.tagDefinitionMoved, tagDefinitionMovedListener);
+
 		propertyDefinitionChangedListener = new PropertyChangeListener() {
 			
 			@SuppressWarnings("unchecked")
@@ -1214,17 +1254,20 @@ public class AnnotationPanel extends VerticalLayout {
 	}
 	
 	public void setTagsets(Collection<TagsetDefinition> tagsets) {
-		tagsets
-		.stream()
-		.filter(tagset -> !this.tagsets.contains(tagset))
-		.forEach(tagset -> addTagset(tagset));
-		
-		this.tagsets.stream()
-		.filter(tagset -> !tagsets.contains(tagset))
-		.collect(Collectors.toList())
-		.stream()
-		.forEach(tagset -> removeTagset(tagset));
+		this.tagsets.clear();
+		this.tagsets.addAll(tagsets);
+		tagsetData.clear();
+		for (TagsetDefinition tagset : tagsets) {
+			TagsetDataItem tagsetItem = new TagsetDataItem(tagset);
+			tagsetData.addItem(null, tagsetItem);
+			addTags(tagsetItem, tagset);
+		}
+		tagsetDataProvider.refreshAll();
+		for (TagsetDefinition tagset : this.tagsets) {
+			expandTagsetDefinition(tagset);
+		}
 	}
+
 
 	public void addTagset(TagsetDefinition tagset) {
 		tagsets.add(tagset);
