efa20eb5802737e5342a88827408f18003139e4d
ypislon <i.vondetten@posteo.de>
2026-07-01 13:26:49 +0200

    version bump to 7.3.1

--- src/main/java/de/catma/ui/util/Version.java
+++ src/main/java/de/catma/ui/util/Version.java
@@ -13,10 +13,10 @@ public final class Version {
     private int patch;
 
 
-    public final static Version CATMA_7_3_0 = new Version(7, 3, 0);
+    public final static Version CATMA_7_3_1 = new Version(7, 3, 1);
 
 
-    public final static Version LATEST = CATMA_7_3_0;
+    public final static Version LATEST = CATMA_7_3_1;
 
 
     /**
